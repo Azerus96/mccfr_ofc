5d9db1412291729f84fa49d98f7ba42a0df6d144
Azerus96 <gencinski1996@gmail.com>
2025-06-25 05:25:56 +0300

    Update game_state.cpp
    Отладочная

--- cpp_src/game_state.cpp
+++ cpp_src/game_state.cpp
@@ -1,7 +1,8 @@
 #include "game_state.hpp"
 
 namespace ofc {
-    // Это определение выделяет память для статической переменной rng_
-    // и инициализирует ее.
-    std::mt19937 GameState::rng_(std::random_device{}());
+    // === ИЗМЕНЕНИЕ ЗДЕСЬ ===
+    // Определяем thread_local статическую переменную.
+    // Она будет инициализирована один раз для каждого потока.
+    thread_local std::mt19937 GameState::rng_(std::random_device{}());
 }
