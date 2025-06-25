052613815e357e4f4204cfe0c676b534d02410db
Azerus96 <gencinski1996@gmail.com>
2025-06-25 06:19:38 +0300

    Update game_state.cpp
    Убрали многопоточность

--- cpp_src/game_state.cpp
+++ cpp_src/game_state.cpp
@@ -1,8 +1,6 @@
 #include "game_state.hpp"
 
 namespace ofc {
-    // === ИЗМЕНЕНИЕ ЗДЕСЬ ===
-    // Определяем thread_local статическую переменную.
-    // Она будет инициализирована один раз для каждого потока.
-    thread_local std::mt19937 GameState::rng_(std::random_device{}());
+    // === ВОЗВРАЩАЕМ ОБЫЧНОЕ ОПРЕДЕЛЕНИЕ ===
+    std::mt19937 GameState::rng_(std::random_device{}());
 }
