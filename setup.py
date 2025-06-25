fa0fb3e378e1e0d79e27dab4763c000f9be1fbfc
Azerus96 <gencinski1996@gmail.com>
2025-06-25 06:36:34 +0300

    Update setup.py
    Отладка

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import numpy
 import glob
 
 # Находим все C++ исходники, которые нужно скомпилировать
+# Мы берем их напрямую из скопированной библиотеки OMPEval
 ompeval_sources = glob.glob("cpp_src/ompeval/omp/*.cpp")
 
 # Наши собственные исходники
@@ -14,14 +15,14 @@ ofc_sources = [
 # Наша Cython-обертка
 cython_ext = Extension(
     "ofc_bot.solver",
-    sources=["ofc_bot/solver.pyx"] + ompeval_sources + ofc_sources,
+    sources=["ofc_bot/solver.pyx"] + ompeval_sources + ofc_sources, # Объединяем все исходники
     include_dirs=[
         numpy.get_include(),
         "cpp_src",
         "cpp_src/ompeval"
     ],
     language="c++",
-    # === УБИРАЕМ ФЛАГИ OPENMP ===
+    # === УБРАНЫ ФЛАГИ OPENMP ДЛЯ ОТЛАДКИ ===
     extra_compile_args=["-std=c++17", "-O3", "-march=native"],
     extra_link_args=[]
 )
